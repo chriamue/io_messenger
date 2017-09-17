64fac24a1007358ac606af51d583fc512b33a844
Christian <chriamue@gmail.com>
2017-09-17 12:48:34 +0200

    added iota lib

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { NavController } from 'ionic-angular';
 
 import { Storage } from '@ionic/storage';
 
+import {IOTA} from 'iota.lib.js';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,8 +13,9 @@ export class HomePage {
   seed: string
   add_address = ""
   addresses = []
+  transferList = []
 
-  constructor(public navCtrl: NavController, private storage: Storage) {
+  constructor(public navCtrl: NavController, private storage: Storage, private iota: IOTA) {
     this.init();
   }
 
@@ -22,6 +25,7 @@ export class HomePage {
       if(JSON.parse(localStorage.getItem("addresses")))
         this.addresses = JSON.parse(localStorage.getItem("addresses"));
     });
+    this.getTransactions();
   }
 
   addAddress() {
@@ -32,5 +36,49 @@ export class HomePage {
 
   chat(address){
     
+  }
+
+  getTransactions(){
+    var checkedTxs = 0
+    var address
+    this.iota.api.getAccountData(this.seed, function (e, accountData) {
+      console.log('Account data', accountData)
+  
+      // Update address in case it's not defined yet
+      if (!address && accountData.addresses[0]) {
+        address = this.iota.utils.addChecksum(accountData.addresses[accountData.addresses.length - 1])
+        console.log(address)
+      }
+  
+      var transferList = []
+  
+      //  Go through all transfers to determine if the tx contains a message
+      //  Only valid JSON data is accepted
+      if (accountData.transfers.length > checkedTxs) {
+        console.log('RECEIVED NEW TXS')
+  
+        accountData.transfers.forEach(function (transfer) {
+          try {
+            var message = this.iota.utils.extractJson(transfer)
+            console.log('Extracted JSON from Transaction: ', message)
+  
+            message = JSON.parse(message)
+            console.log('JSON: ', message)
+  
+            var newTx = {
+              'name': message.name,
+              'message': message.message,
+              'value': transfer[0].value
+            }
+            transferList.push(newTx)
+          } catch(e) {
+            console.log('Transaction did not contain any JSON Data')
+          }
+        })
+  
+        // Increase the counter of checkedTxs
+        checkedTxs = accountData.transfers.length
+      }
+    })
   }
 }
